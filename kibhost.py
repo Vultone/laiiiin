644f99eef16b617ff014e58eff4bbece7ad53de9
Vultone <42094673+Vultone@users.noreply.github.com>
2018-09-09 12:38:15 -0300

    Update kibhost.py

--- kibhost.py
+++ kibhost.py
@@ -11,13 +11,111 @@ client = commands.Bot(command_prefix='kiibo')
 async def on_ready():
     print('client KiiboBot ready')
     await client.change_presence(game = discord.Game(
-        name = 'If you see this message, I work!'
+        name = 'Bots don\'t need sleep!'
     ))
 
 @client.command()
-async def hi():
-    await client.say('Hello! I\'m being hosted somewhere else right now!')
+async def die():
+    await client.say('no u \n https://tinyurl.com/kiibo1')
+    
+@client.command()
+async def ahegao():
+    await client.say('Uwaaah~~~!!!!! \n https://tinyurl.com/kiibo2')
 
+@client.command()
+async def tee():
+    await client.say("hee!!")
+
+@client.command()
+async def sucks():
+    await client.say('no u \n https://tinyurl.com/kiibo1')
+
+@client.command()
+async def nukeiran():
+    await client.say('Okay, for how long?')
+
+@client.command()
+async def playdespacito():
+    await client.say('**Aye.** \n https://www.youtube.com/watch?v=kJQP7kiw5Fk')
+    
+@client.command()
+async def imtired():
+    await client.say('/Tuck in')
+@client.command()
+async def imsleepy():
+    await client.say('/Tuck in')
+@client.command()
+async def imgoingtobed():
+    await client.say('/Tuck in')
+    
+@client.command()
+async def pats():
+    await client.say('https://tinyurl.com/kiibo3')
+
+@client.command()
+async def goodnight():
+    await client.say('Good night! \nhttps://tinyurl.com/kiibobed')
+
+@client.command()
+async def goodmorning():
+    await client.say('Good morning! \n https://tinyurl.com/kiibo55')
+
+@client.command()
+async def mineforme():
+    await client.say('GIVE ME THE GOLD! **WHERE\'S THE GOLD!!!** \n https://tinyurl.com/kiibomine')
+
+@client.command()
+async def whosthere():
+    await client.say('**MASSIVE LEGEND HERE** \nhttps://tinyurl.com/kiblegend')
+
+@client.command()
+async def detect():
+    await client.say('Gamer detected.\nhttps://tinyurl.com/kibdetect')
+
+@client.command()
+async def rocketpunch():
+    await client.say('**WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW**\nhttps://tinyurl.com/kibpunch')
+    
+@client.command()
+async def helpbronte():
+    await client.say('not even god can help bronte now')
+    
+@client.command()
+async def riseup():
+    await client.say('**RISING UP INITIATED.**')
+    
+@client.command()
+async def hi():
+    await client.say('Hello!')
+
+@client.command()
+async def representnewbrunswick():
+    await client.say('https://tinyurl.com/newbrunsw')
+
+@client.command()
+async def suckedmygootuberbehindarbys():
+    await client.say('∠( \'ω\')／ Maybe ＼(\'ω\' )>﻿')
+    
+@client.command()
+async def fuckme():
+    await client.say('My dick fell off!\nhttps://tinyurl.com/ybgygnkk')
+
+@client.command()
+async def chokeme():
+    await client.say('https://tinyurl.com/ychvpbdu')
+    
+@client.command()
+async def blessrichie():
+    await client.say('We out here for u richie. this 2018 we stan a furry king')
+    
+@client.command()
+async def wishthismanabirthday():
+    await client.say(':balloon: :balloon: **HAPPY BIRTHDAY SHUICHI** :balloon: :balloon: \nhttps://tinyurl.com/kibbirth')
+
+@client.command()
+async def info():
+    await client.say('This bot was made by wren#5626. Commands are subject to frequent change. Do `kiibo help` for a list of options. epic')
+    
 @client.event
 async def on_message(message):
     message.content = message.content.lower().replace(' ', '').replace('\'','').replace('\u2019','')
