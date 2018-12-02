b7b1355036019c01696b758cc364b2152f338940
Wren Swim <42094673+Vultone@users.noreply.github.com>
2018-12-02 14:45:17 -0400

    Update kibhost.py

--- kibhost.py
+++ kibhost.py
@@ -7,6 +7,9 @@ import os
 
 client = commands.Bot(command_prefix='kiibo')
 
+pistolwhip = discord.Embed()
+pistolwhip.set_image(url="https://cdn.discordapp.com/attachments/475075193690390528/498297865068871715/-0.png")
+
 @client.event
 async def on_ready():
     print('client KiiboBot ready')
@@ -114,7 +117,7 @@ async def awoo():
 
 @client.command()
 async def killthisfool():
-    await client.say('Get ready for Kiibo\'s Pistol Whippin\' .45! **SAIONARA!**{files: ["https://cdn.discordapp.com/attachments/475075193690390528/498297865068871715/-0.png"]}')
+    await client.say('Get ready for Kiibo\'s Pistol Whippin\' .45! **SAIONARA!**',pistolwhip)
     
 @client.command()
 async def sendbirthdaywishes():
